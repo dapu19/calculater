b8c9947cd32c1f6e559e23ff62d58b6ae84dcb53
david <dapu19@gmail.com>
2019-11-18 15:59:25 +0000

    Started CG

---
+++ CG.c
@@ -0,0 +1,5 @@
+//
+// Created by david on 18/11/2019.
+//
+
+#include "CG.h"
--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int VM(char arr[120], int size);
 
 int main() {
     printf("Hello, World!\n");
-    char sum[][40] = { "loadint 5", "loadint 7","add","loadint 6","loadint 2", "sub", "mul"};
+    char sum[][40] = { "loadint 5", "loadint 3457","add","loadint 6","loadint 2", "sub", "mul"};
 
     int result = VM(sum, 7);
     printf("result = %d", result);
