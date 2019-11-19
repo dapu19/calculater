716d5a9f03eeeeec75229b17f1f739f1ef6d9552
david <dapu19@gmail.com>
2019-11-19 19:58:45 +0000

    Maybe got tok working

--- tok.c
+++ tok.c
@@ -18,7 +18,7 @@ void Tok(){
     fp = fopen("C:\\Users\\david\\CLionProjects\\calculater\\file.txt", "r");
 
     char ch ;
-    char sum[100][240];
+    char sum[200][4];
     int i = 0;
     bool number = false;
 
@@ -31,47 +31,59 @@ void Tok(){
         char * strs = "";
         char * tempnum = "";
 
+
         while ((ch = fgetc(fp)) != EOF)
         {
 
             char str = ch;
+            char * ops = "";
 
 
 
             if (strncmp(&ch,"+", 1) == 0  || strncmp(&ch,"-", 1) == 0 || strncmp(&ch,"*", 1) == 0 ||
             strncmp(&ch,"/", 1) == 0 || strncmp(&ch,"(", 1) == 0 || strncmp(&ch,")", 1) == 0){
-                printf("\n operator");
+
+
                 if (number){
                     number = false;
 
+                    printf("\n %s \n", tempnum);
+                    strcpy(sum[i], tempnum);
+                    i++;
+                    memset(tempnum, 0, sizeof(tempnum));
+
                 }
+
+
+                printf("\n operator \n");
+                size_t len = strlen(ops);
+                char *str2 = malloc(len + 1 + 1);
+                strcpy(str2, ops);
+                str2[len] = str;
+                str2[len + 1] = '\0';
+
+
+                strcpy(sum[i], str2);
+                free(str2);
+                i++;
             }else{
-                if (number) {
-                    size_t len = strlen(strs);
-                    char *str2 = malloc(len + 1 + 1);
-                    strcpy(str2, strs);
-                    str2[len] = str;
-                    str2[len + 1] = '\0';
-                    //printf("\n %c", str);
-
-                    //printf("\n %s", str2);
-                    strcat(tempnum, str2);
-                    free(str2);
-                    printf(tempnum);
-                }else{
+                if (!number) {
                     number = true;
-                    size_t len = strlen(strs);
-                    char *str2 = malloc(len + 1 + 1);
-                    strcpy(str2, strs);
-                    str2[len] = str;
-                    str2[len + 1] = '\0';
-
-                    strcat(tempnum, str2);
-
-                    free(str2);
-                    printf(tempnum);
-
                 }
+
+
+                size_t len = strlen(strs);
+                char *str2 = malloc(len + 1 + 1);
+                strcpy(str2, strs);
+                str2[len] = str;
+                str2[len + 1] = '\0';
+
+                strcat(tempnum, str2);
+
+                free(str2);
+
+
+
             }
 
 
@@ -81,7 +93,12 @@ void Tok(){
         }
 
         printf("Closing the file \n");
+
         fclose(fp);
+
+        for(int j = 0; j < i; j++){
+            printf("%s \n ", sum[j]);
+        }
     }
 
 
