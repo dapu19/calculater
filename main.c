92d8046fc416b2437563b96bda77cc2dae8a3238
Matt-Parker99 <mattie2011.mp@gmail.com>
2019-11-19 22:03:00 +0000

    Completely changed i2p, needs to be updated to take out # and add to 2d list yet

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 
 void CG(char arr[120][40], int size);
 int VM(char arr[120], int size);
-void Infix2Postfix(char in[10][240], char post[10][240]);
 char * Tok();
 
 
@@ -17,10 +16,6 @@ int main() {
     char Mattsum[][40] = { "5", "3457","+","6","2", "-", "*"};
 
     CG(Mattsum, 7);
-
-    char MatthewsAwesomeInfixArray[][1] = {"1", "+", "2"};
-    char MatthewsAwesomePostfixArray[100][240];
-    //Infix2Postfix(MatthewsAwesomeInfixArray, MatthewsAwesomePostfixArray);
 
     return 0;
 }
